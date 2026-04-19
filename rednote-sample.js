53844cc5c34de559fb5fd8d8d6f8821f2ae72944
ritsusaku <snellypizza@gmail.com>
2026-04-19 11:02:40 -0400

    Create rednote-sample.js

---
+++ rednote-sample.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         fix xhs sample
+// @namespace    http://tampermonkey.net
+// @author       ritsusaku
+// @version      922
+// @description  redirect to xhscdn not rednote
+// @match        *://*.rednotecdn.com/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+  /*
+  cdns
+  https://sns-img-hw.xhscdn.com/
+  https://sns-img-al.xhscdn.com/
+  https://ci.xiaohongshu.com/
+  */
+
+  (function () {
+  'use strict';
+  const xhscdn = 'https://sns-img-hw.xhscdn.com/';
+  const prepost = location.href.match(/notes_pre_post\/([^!?&\s]+)/);
+  const simple = location.href.match(/\/([^/!?&\s]+)!/)
+  if (prepost) location.replace(xhscdn + 'notes_pre_post/' + prepost[1]);
+  else if (simple) location.replace(xhscdn + simple[1]);
+})();
