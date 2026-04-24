c68a57813922409bc17bcc99305ccc8fc9540d8f
Vera <snellypizza@gmail.com>
2026-04-24 15:55:21 -0400

    Add oss/note/ support

--- rednote-sample.js
+++ rednote-sample.js
@@ -2,7 +2,7 @@
 // @name         fix xhs sample
 // @namespace    http://tampermonkey.net
 // @author       ritsusaku
-// @version      922
+// @version      922.1
 // @description  redirect to xhscdn not rednote
 // @match        *://*.rednotecdn.com/*
 // @grant        none
@@ -21,6 +21,8 @@
   const xhscdn = 'https://sns-img-hw.xhscdn.com/';
   const prepost = location.href.match(/notes_pre_post\/([^!?&\s]+)/);
   const simple = location.href.match(/\/([^/!?&\s]+)!/)
+  const osssg = location.href.match(/oss-sg\/notes\/([^!?&\s]+)/);
   if (prepost) location.replace(xhscdn + 'notes_pre_post/' + prepost[1]);
+  else if (osssg) location.replace(xhscdn + 'oss-sg/notes/' + osssg[1]);
   else if (simple) location.replace(xhscdn + simple[1]);
 })();
