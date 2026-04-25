910f1f9a58ff3ca864ea36055f9f936715942d25
Vera <snellypizza@gmail.com>
2026-04-25 16:01:56 -0400

    🤦

--- rednote-sample.js
+++ rednote-sample.js
@@ -2,7 +2,7 @@
 // @name         fix xhs sample
 // @namespace    http://tampermonkey.net
 // @author       ritsusaku
-// @version      922.1
+// @version      922.2
 // @description  redirect to xhscdn not rednote
 // @match        *://*.rednotecdn.com/*
 // @grant        none
@@ -22,7 +22,9 @@
   const prepost = location.href.match(/notes_pre_post\/([^!?&\s]+)/);
   const simple = location.href.match(/\/([^/!?&\s]+)!/)
   const osssg = location.href.match(/oss-sg\/notes\/([^!?&\s]+)/);
+  const spctrum = location.href.match(/spectrum\/([^!?&\s]+)/);
   if (prepost) location.replace(xhscdn + 'notes_pre_post/' + prepost[1]);
   else if (osssg) location.replace(xhscdn + 'oss-sg/notes/' + osssg[1]);
-  else if (simple) location.replace(xhscdn + simple[1]);
+  else if (spctrum) location.replace(xhscdn + 'spectrum/' + spctrum[1]);
+      else if (simple) location.replace(xhscdn + simple[1]);
 })();
